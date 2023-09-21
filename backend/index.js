dbbfb04570c7eccdf00e4350f4f9da8a145b0352
Bazell Partridge <elbitapmoC@users.noreply.github.com>
2023-09-21 08:05:06 -0400

    save

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
-
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.post("/autheticate", async (req, res) => {
-  const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+app.post("/auth", async (req, res) => {
+  const { username, secret } = req.body;
+
+  // Fetch this user from Chat Engine in this project!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "a101c59d-07cc-4fe9-ab1d-f0bca2d11bdc" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
 app.listen(3001);
